779daec20617bcf1f10d425f97486a49152a4e1c
Sergio Aaron Nungaray Arteaga <65799736+SergioAaron@users.noreply.github.com>
2022-08-10 15:53:11 -0500

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LogIN</title>
+</head>
+
+<body>
+
+    <form action="#" method="POST" class="formulario__login">
+        <h1>Bienvenido</h1>
+        <h2>Usuario</h2>
+        <input type="text" placeholder="Usuario" name="Usuario">
+        <h2>Contraseña</h2>
+        <input type="password" placeholder="Contraseña" name="contrasena">
+    </form>
+</body>
+</html>
